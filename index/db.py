1f876b46d473e7f575ed580c46d5759de503cf4c
ChangRon <changrontw@gmail.com>
2017-03-07 15:00:12 +0800

    add comments in index/views and db.py and sql.py

--- index/db.py
+++ index/db.py
@@ -1,11 +1,11 @@
 """
-Data visualization
+Data visualization -- retrieve data only
 """
-def get_all_table_names():
+def get_all_table_names(database):
     table_names = ['Round Table','Square table','Triangle Table']
     return table_names
 
-def get_table(table_name):
+def get_table(table_name, database):
     title = ['title1','title2']
     content = [['row1 content1','row1 content2'],['row2 content1','row2 content2']]
     return title, content
@@ -14,5 +14,5 @@ def get_table(table_name):
 Data modification
 """
 
-def create_table(table_name):
+def create_table(table_name, database):
     return False
--- index/views.py
+++ index/views.py
@@ -7,17 +7,22 @@ def index(request):
     return render(request,'index/index.html')
 
 def sql_view(request):
+    # to store input sql, make sure we receive the right input
     sql_str = "Input SQL will be shown here"
 
     if request.method == 'GET':
-        data = {'sql':sql_str
-            }
+        data = {'sql':sql_str}
         return render(request,'index/sql.html', data)
     elif request.method == 'POST':
         if request.POST.get('sql'):
             sql_str = request.POST['sql']
+        # the db we want to do action on
         database = None
+        # apply sql to the database
+        # (Bool,String) to indicate status of execution and error message
         success, err_msg = sql.exec_sql(sql_str, database)
+
+        # additional message to indicate the execution is successful or not
         panel_msg = ""
         if success:
             panel_msg += "success"
@@ -33,14 +38,19 @@ def sql_view(request):
         return render(request,'index/sql.html', data)
 
 def table_view(request,table_name=None):
-    table_names = db.get_all_table_names()
+    # the db we want to view
+    database = None
+    # retreive all table names
+    table_names = db.get_all_table_names(database)
+
+    # if user doesn't specify which table to view, choose the first one as default
     if table_name == None:
         try:
             table_name = table_names[0]
         except:
             table_name = "Default table name"
     
-    title, content = db.get_table(table_name)
+    title, content = db.get_table(table_name, database)
     data = {'table_names':table_names,
             'table_name':table_name,
             'title':title,
