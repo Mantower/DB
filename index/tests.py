80cf82da83d2639955c722d5a8e43575b4aa21c8
ChangRon <changrontw@gmail.com>
2017-03-13 10:58:44 +0800

    add more table creation testcase

--- index/tests.py
+++ index/tests.py
@@ -18,20 +18,73 @@ def load_db():
 
 # Create your tests here.
 class TableTestCase(TestCase):
+    # We can always start from clean Database if we don't save our Database
     def setUp(self):
         database = miniDB.Database()
         save_db(database)
 
     def test_table_creation(self):
+        # test table creation 
         database = load_db()
         sql1 = "CREATE TABLE STUDENT (\
-        studentId int PRIMARY KEY,\
-        name VARCHAR(15),\
-        gender VARCHAR(1),\
-        age int\
-        )"
+            studentId int PRIMARY KEY,\
+            name VARCHAR(15),\
+            gender VARCHAR(1),\
+            age int\
+            )"
         passed, err_msg = database.exec_sql(sql1)
         self.assertEqual(passed, [True])
         self.assertEqual(err_msg, [None])
 
+    def test_table_creation_duplicate(self):
+        # test table creation with already exists name
+        database = load_db()
+        sql1 = "CREATE TABLE STUDENT (\
+            studentId int PRIMARY KEY,\
+            name VARCHAR(15),\
+            gender VARCHAR(1),\
+            age int\
+            );"
+        passed, err_msg = database.exec_sql(sql1+sql1)
+        self.assertEqual(passed, [True, False])
+        self.assertEqual(err_msg, [None, "Table with same name exists."])
+
+    def test_table_creation2(self):
+        database = load_db()
+        sql = "CREATE TABLE VEHICLE (\
+            licenseNumber VARCHAR(10),\
+            brand VARCHAR(15),\
+            model VARCHAR(15),\
+            type VARCHAR(2),\
+            engineSize int\
+            );"
+        passed, err_msg = database.exec_sql(sql)
+        self.assertEqual(passed, [True])
+        self.assertEqual(err_msg, [None])
+    
+    def test_table_creation3(self):
+        database = load_db()
+        sql = "CREATE TABLE BOOK (\
+        isbn VARCHAR(20) PRIMARY KEY,\
+        title VARCHAR(20),\
+        author VARCHAR(20),\
+        pages int,\
+        editorial VARCHAR(15)\
+        )"
+        passed, err_msg = database.exec_sql(sql)
+        self.assertEqual(passed, [True])
+        self.assertEqual(err_msg, [None])
+
+    def test_table_creation_with_2pks(self):
+        database = load_db()
+        sql = "CREATE TABLE COURSE (\
+            name VARCHAR(40) PRIMARY KEY,\
+            id INT PRIMARY KEY\
+            )"
+        passed, err_msg = database.exec_sql(sql)
+        self.assertEqual(passed, [True])
+        self.assertEqual(err_msg, [None]) 
+
+
+    
         
