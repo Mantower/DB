f9732ca252534f3a34038535a90378d62e7a24b2
ChangRon <changrontw@gmail.com>
2017-03-13 11:17:08 +0800

    add more testcase, and create new database with student table in it.

--- index/tests.py
+++ index/tests.py
@@ -5,52 +5,65 @@ import miniDB
 #https://docs.djangoproject.com/en/1.10/topics/testing/overview/
 
 TEST_DB_NAME = "TESTING.pkl"
+TEST_DB_WITH_STUDENT = "TESTING_STUDENT.pkl"
 
-def save_db(database):
+def save_db(database, db_name):
     # dump new db into a file
-    output = open(TEST_DB_NAME, 'wb')
+    output = open(db_name, 'wb')
     pickle.dump(database, output)
 
-def load_db():
+def load_db(db_name):
     # read DB from pkl file
-    with open(TEST_DB_NAME, 'rb') as f:
+    with open(db_name, 'rb') as f:
         return pickle.load(f)
 
 # Create your tests here.
 class TableTestCase(TestCase):
     # We can always start from clean Database if we don't save our Database
+    # valie Table name character, A-Z, 0-9 and _
     def setUp(self):
         database = miniDB.Database()
-        save_db(database)
+        save_db(database, TEST_DB_NAME)
 
-    def test_table_creation(self):
-        # test table creation 
-        database = load_db()
-        sql1 = "CREATE TABLE STUDENT (\
+        database_with_student_table = miniDB.Database()
+        database = load_db(TEST_DB_NAME)
+        sql = "CREATE TABLE STUDENT (\
             studentId int PRIMARY KEY,\
             name VARCHAR(15),\
             gender VARCHAR(1),\
             age int\
             )"
-        passed, err_msg = database.exec_sql(sql1)
+        passed, err_msg = database_with_student_table .exec_sql(sql)
+        save_db(database, TEST_DB_WITH_STUDENT)
+
+    def test_table_creation(self):
+        # test table creation 
+        database = load_db(TEST_DB_NAME)
+        sql = "CREATE TABLE STUDENT (\
+            studentId int PRIMARY KEY,\
+            name VARCHAR(15),\
+            gender VARCHAR(1),\
+            age int\
+            )"
+        passed, err_msg = database.exec_sql(sql)
         self.assertEqual(passed, [True])
         self.assertEqual(err_msg, [None])
 
     def test_table_creation_duplicate(self):
         # test table creation with already exists name
-        database = load_db()
-        sql1 = "CREATE TABLE STUDENT (\
+        database = load_db(TEST_DB_NAME)
+        sql = "CREATE TABLE STUDENT (\
             studentId int PRIMARY KEY,\
             name VARCHAR(15),\
             gender VARCHAR(1),\
             age int\
             );"
-        passed, err_msg = database.exec_sql(sql1+sql1)
+        passed, err_msg = database.exec_sql(sql+sql)
         self.assertEqual(passed, [True, False])
         self.assertEqual(err_msg, [None, "Table with same name exists."])
 
     def test_table_creation2(self):
-        database = load_db()
+        database = load_db(TEST_DB_NAME)
         sql = "CREATE TABLE VEHICLE (\
             licenseNumber VARCHAR(10),\
             brand VARCHAR(15),\
@@ -63,7 +76,7 @@ class TableTestCase(TestCase):
         self.assertEqual(err_msg, [None])
     
     def test_table_creation3(self):
-        database = load_db()
+        database = load_db(TEST_DB_NAME)
         sql = "CREATE TABLE BOOK (\
         isbn VARCHAR(20) PRIMARY KEY,\
         title VARCHAR(20),\
@@ -76,7 +89,7 @@ class TableTestCase(TestCase):
         self.assertEqual(err_msg, [None])
 
     def test_table_creation_with_2pks(self):
-        database = load_db()
+        database = load_db(TEST_DB_NAME)
         sql = "CREATE TABLE COURSE (\
             name VARCHAR(40) PRIMARY KEY,\
             id INT PRIMARY KEY\
@@ -84,6 +97,57 @@ class TableTestCase(TestCase):
         passed, err_msg = database.exec_sql(sql)
         self.assertEqual(passed, [True])
         self.assertEqual(err_msg, [None]) 
+
+    def test_table_creation_with_missing_paranthesis(self):
+        database = load_db(TEST_DB_NAME)
+        sql = "CREATE TABLE TEACHERS\
+        FName VARCHAR,\
+        LName VARCHAR(20)\
+        )"
+        passed, err_msg = database.exec_sql(sql)
+        self.assertEqual(passed, [False])
+        #self.assertEqual(err_msg, [None]) 
+
+    def test_table_creation_with_unknown_type(self):
+        database = load_db(TEST_DB_NAME)
+        sql = "CREATE TABLE TEACHERS (\
+        FName VARCHAR(1),\
+        LName DATE\
+        )"
+        passed, err_msg = database.exec_sql(sql)
+        self.assertEqual(passed, [False])
+        #self.assertEqual(err_msg, [None]) 
+
+    def test_table_creation_with_extra_comma(self):
+        database = load_db(TEST_DB_NAME)
+        sql = "CREATE TABLE TEACHERS (\
+            FName VARCHAR(4),\
+            LName VARCHAR(4),\
+            )"
+        passed, err_msg = database.exec_sql(sql)
+        self.assertEqual(passed, [False])
+        #self.assertEqual(err_msg, [None])
+
+    def test_table_creation_with_duplicate_column_name(self):
+        database = load_db(TEST_DB_NAME)
+        sql = "CREATE TABLE TEACHERS (\
+            FName VARCHAR,\
+            LName VARCHAR,\
+            Fname VARCHAR\
+            )"
+        passed, err_msg = database.exec_sql(sql)
+        self.assertEqual(passed, [False])
+        #self.assertEqual(err_msg, [None])
+
+    def test_table_creation_with_bounds_varchar_limit(self):
+        database = load_db(TEST_DB_NAME)
+        sql = "CREATE TABLE TEACHERS (\
+            FName VARCHAR,\
+            LName VARCHAR(50)\
+            )"
+        passed, err_msg = database.exec_sql(sql)
+        self.assertEqual(passed, [False])
+        #self.assertEqual(err_msg, [None])
 
 
     
