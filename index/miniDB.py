3120750abb7d6f5a51c918fc04abcad982160ac2
ChangRon <changrontw@gmail.com>
2017-04-08 15:01:41 +0800

    fix bracket error

--- index/miniDB.py
+++ index/miniDB.py
@@ -181,7 +181,7 @@ class Database:
         column_objs = []
         if column_names == '*':
             for idx, t in enumerate(tables_obj):
-                for cid, col in enumerate(self.tables[tid].columns)):
+                for cid, col in enumerate(self.tables[tid].columns):
                     column_infos.append((idx, cid, None))
                     column_objs.append(col)
         else:
@@ -480,8 +480,6 @@ class Predicate:
             String: The error message. None if no error.
         """
         pass
-    
-
 
 """
 Temporary functions that insert fake data into views
